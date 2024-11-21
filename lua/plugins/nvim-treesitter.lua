26fe31029679648f89a9ed9ce3a86c86bb9deb40
xuhangyu.12123 <xuhangyu.12123@bytedance.com>
2024-11-21 20:13:09 +0800

    add treesitter for each language
    Change-Id: Ic72780af9899cae7c99f0a077581a795a4b7586b

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "go",
+      "javascript",
+      "typescript",
+      "tsx",
+      "ruby",
+      "lua",
+      "python",
+      "bash",
+      "json",
+      "html",
+      "xml",
+      "yaml",
+      "toml",
+    },
+  },
+}
